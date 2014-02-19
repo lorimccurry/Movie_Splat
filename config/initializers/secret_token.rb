c9f0db860b92b64f68b4cca550ac0e41d1cce570
Lori McCurry <lori.mccurry@gmail.com>
2014-02-19 14:42:52 -0600

    Initialize rails: `rails new . -T --database=postgresql`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenTomatoesCapstone::Application.config.secret_key_base = 'fc61da18ad856af62b397b2cb0913a10b0e6e5c70e18ce719f791ff34d154aa7447d29ae3d9ceaa744a77d7ed6551b600b29e40bb9d107178bbb7df673728131'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenTomatoesCapstone::Application.config.session_store :cookie_store, key: '_rotten_tomatoes_capstone_session'
