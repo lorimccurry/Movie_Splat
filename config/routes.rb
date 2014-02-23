a58bef07db5ac7d6cc5a91ce647928360309004e
Lori McCurry <lori.mccurry@gmail.com>
2014-02-22 18:26:00 -0600

    Change home to movies controller `rails g controller movies`

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ RottenTomatoesCapstone::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root:to => 'home#home'
+  root:to => 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
