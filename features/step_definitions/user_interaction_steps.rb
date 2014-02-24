a200f01c76ba58baa282853af43565f158a36b19
Lori McCurry <lori.mccurry@gmail.com>
2014-02-24 13:41:42 -0600

    Add sign in step definition

---
+++ features/step_definitions/user_interaction_steps.rb
@@ -0,0 +1,60 @@
+And "show me the page" do
+  save_and_open_page
+end
+
+When(/^I go to the home page$/) do
+  visit root_path
+end
+
+When(/^I (?:click|follow) "(.*?)"$/) do |link|
+  click_link(link)
+end
+
+When(/^I fill in "(.*?)" for "(.*?)"$/) do |content, field|
+  fill_in(field, with: content)
+end
+
+When(/^I press "(.*?)"$/) do |text|
+  click_button(text)
+end
+
+Then(/^I should see(?::)? "(.*?)"$/) do |text|
+  page.should have_content(text)
+end
+
+Then(/^I should not see "(.*?)"$/) do |text|
+  page.should_not have_content(text)
+end
+
+Given(/^the user "(.*?)" with "(.*?)"$/) do |email, password|
+  User.create(email: email, password: password, password_confirmation: password)
+end
+
+Given(/^I am on the sign in page$/) do
+  visit(user_session_path)
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, content|
+  fill_in(field, with: content)
+end
+
+When(/^I sign up$/) do
+  steps %Q{
+    When I go to the home page
+    And I follow "Sign Up"
+    And I fill in "joe@example.com" for "Email"
+    And I fill in "password" for "user_password"
+    And I fill in "password" for "Password confirmation"
+    And I press "Sign up"
+  }
+end
+
+When(/^I sign in$/) do
+  steps %Q{
+    When I go to the home page
+    And I follow "Sign In"
+    And I fill in "joe@example.com" for "Email"
+    And I fill in "password" for "user_password"
+    And I press "Sign in"
+  }
+end
