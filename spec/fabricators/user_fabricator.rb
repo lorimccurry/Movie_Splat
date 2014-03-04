ea34b7f69e87c41a3358e2148ff55dac06847096
John Bohn <jjbohn@gmail.com>
2014-03-04 14:46:53 -0600

    Use Amy fabrictor to create test user

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,6 @@
-Fabricator(:user) do
-  email { sequence(:email) { |i| "user#{i}@example.com" } }
+Fabricator(:user, aliases: [:amy]) do
+  username "amy"
+  email "amy@example.com"
   password "password"
   password_confirmation "password"
 end
