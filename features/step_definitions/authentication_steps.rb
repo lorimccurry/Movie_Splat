d6d5bd394215d2be7666bb24900f0f0fa95634ac
Lori McCurry <lori.mccurry@gmail.com>
2014-03-04 21:21:20 -0600

    Use Amy fabricator to create test user

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,16 +1,12 @@
 Given(/^the user "(.*?)"\/"(.*?)" with "(.*?)"$/) do |username, email, password|
-  # Fabricate(:user, username: username, email: email, password: password_confirmation, password_confirmation: password)
-  User.create(username: username, email: email, password: password, password_confirmation: password)
+  Fabricate(:user, username: username, email: email, password: password, password_confirmation: password)
 end
 
 Given(/^I'm signed in as "(.*?)"$/) do |username|
-  # Fabricate(:user, username: username)
-  email = "#{username}@#{username}.org"
-  User.create(username: username, email: email, password: "password", password_confirmation: "password")
+  user = Fabricate(username)
   visit new_user_session_path
-  fill_in "Email / Username", with: email
-  # fill_in "Email / Username", with: username
-  fill_in "Password", with: "password"
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
   click_button "Sign in"
   page.should have_content("Signed in successfully")
 end
