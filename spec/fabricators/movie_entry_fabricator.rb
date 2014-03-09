fb84b71aab8b21d2789183479674bf6a1367c190
Lori McCurry <lori.mccurry@gmail.com>
2014-03-08 22:35:15 -0600

    WIP: Splat report basic setup

--- spec/fabricators/movie_entry_fabricator.rb
+++ spec/fabricators/movie_entry_fabricator.rb
@@ -9,3 +9,27 @@ Fabricator(:movie_entry, alias: [:movie_entry_up]) do
   user { Fabricate(:amy) }
   movie { Fabricate("Up") }
 end
+
+Fabricator(:movie_entry, alias: [:movie_entry_gravity]) do
+  seen true
+  own true
+  wishlist_see true
+  wishlist_own true
+  user_rating 90
+  user_comments "I liked it"
+
+  user { Fabricate(:amy) }
+  movie { Fabricate("Gravity") }
+end
+
+Fabricator(:movie_entry, alias: [:movie_entry_american_hustle]) do
+  seen true
+  own true
+  wishlist_see true
+  wishlist_own true
+  user_rating 90
+  user_comments "I liked it"
+
+  user { Fabricate(:amy) }
+  movie { Fabricate("American Hustle") }
+end
